22b8610d17ddbef6ee45b4ed4a27e3c87719d867
Vivek Gupta <vivekgupta@paypal.com>
2016-08-03 18:10:50 +0530

    Program Completed

---
+++ Odometer.java
@@ -0,0 +1,106 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Odometer {
+		
+	public static boolean contains0(long reading) {
+		while(reading > 0) {
+			if(reading % 10 == 0) {
+				return true;
+			}
+			reading /= 10;
+		}
+		return false;
+	}
+	
+	public static boolean isAscendingDigits(long reading) {
+		String readingStr = String.valueOf(reading);
+		for(int i = 0; i < readingStr.length() - 1; i++) {
+			if(readingStr.charAt(i) >= readingStr.charAt(i + 1)) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	public static boolean isCorrectReading(long reading) {
+		return !contains0(reading) && isAscendingDigits(reading);
+	}
+	
+	public static int numOfDigits(long reading) {
+		return String.valueOf(reading).length();
+	}
+	
+	public static long getSmallestReading(int size) {
+		
+		int smallestReading = 0;
+		for(int i = 1; i <= size; i++) {
+			smallestReading = smallestReading * 10 + i;
+		}
+		return smallestReading;
+	}
+	
+	public static long getLargestReading(int size) {
+		int largestReading = 0;
+		for(int i = 9 - size + 1; i <= 9; i++) {
+			largestReading = largestReading * 10 + i;
+		}
+		return largestReading;
+	}
+	
+	public static List<Long> createReading(int size) {
+		List<Long> readingList = new ArrayList<Long>();
+		long smallestReading = getSmallestReading(size);
+		long largestReading = getLargestReading(size);
+		
+		for(long i = smallestReading; i <= largestReading; i++) {
+			if(isCorrectReading(i)) {
+				readingList.add(i);
+			}
+		}
+		return readingList;
+	}
+	
+	public static int getIndex(long reading, List<Long> readingList) {
+		for(int i = 0; i < readingList.size(); i++) {
+			if(readingList.get(i) == reading) {
+				return i;
+			}		
+		}
+		return -1;
+	}
+	
+	public static long nextReading(long reading, List<Long> readingList) {
+		int index = getIndex(reading, readingList);
+		if(index == readingList.size() - 1) {
+			return readingList.get(0);
+		} 
+		return readingList.get(index + 1);
+	}
+	
+	public static long prevReading(long reading, List<Long> readingList) {
+		int index = getIndex(reading, readingList);
+		if(index == 0) {
+			return readingList.get(readingList.size() - 1);
+		} 
+		return readingList.get(index - 1);
+	}
+	
+	public static long distance(long reading1, long reading2, List<Long> readingList) {
+		int index1 = getIndex(reading1, readingList);
+		int index2 = getIndex(reading2, readingList);
+		
+		if(index2 - index1 > 0) {
+			return index2 - index1;
+		} else {
+			return readingList.size() - (index1 - index2);
+		}
+	}
+	
+	
+	public static void main(String args[]) {
+		System.out.println(distance(89L, 12L, createReading(2)));
+	}
+		
+}
